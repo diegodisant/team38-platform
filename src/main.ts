898c0ad701786d0a04a7e2383cddb382655948a0
diegodisant <developerdiego0@gmail.com>
2020-07-29 00:59:53 -0500

    chore(deps): add vuetify support

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import vuetify from './plugins/vuetify';
+
+import '@sweetalert2/theme-dark/dark.css';
+import './scss/app.sass';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  // @ts-ignore
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
